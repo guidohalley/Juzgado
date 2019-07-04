7a4f353043dc85d7fc10183e7f51020c71b4fc7e
guidohalley <guidohalley@hotmail.com>
2019-07-03 21:38:40 -0300

    con creador de carpetas

---
+++ menuprincipal/creadordecarpetas.py
@@ -0,0 +1,20 @@
+import os
+
+def crear_directorio(ruta,ruta1,ruta2, ruta3):
+	try:
+		
+		os.makedirs(ruta)
+		os.makedirs(ruta1)
+		os.makedirs(ruta2)
+		os.makedirs(ruta3)
+		return True
+
+	except :
+		return False
+
+directorio_escritorio = os.environ['USERPROFILE'] + "\\Desktop\\Juzgado de Violencia Familia"
+directorio_escritorio1 = os.environ['USERPROFILE'] + "\\Desktop\\Juzgado de Violencia Familia\\Prohibicion"
+directorio_escritorio2 = os.environ['USERPROFILE'] + "\\Desktop\\Juzgado de Violencia Familia\\Exclusion"
+directorio_escritorio3 = os.environ['USERPROFILE'] + "\\Desktop\\Juzgado de Violencia Familia\\Confirmacion"
+
+crear_directorio(directorio_escritorio, directorio_escritorio1, directorio_escritorio2, directorio_escritorio3)
