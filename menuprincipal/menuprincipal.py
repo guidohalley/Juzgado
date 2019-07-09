5dbd7eb00daced0211cc565638f9186ec43ffdeb
guidohalley <guidohalley@hotmail.com>
2019-07-08 22:05:08 -0300

    creadordecarpetas + arreglo de sexo en Anuncio de Sexo denunciado

--- menuprincipal/menuprincipal.py
+++ menuprincipal/menuprincipal.py
@@ -6,8 +6,7 @@ print("| | | |(_)       | |                   (_)        |  ___|                
 print("| | | | _   ___  | |  ___  _ __    ___  _   __ _  | |_     __ _  _ __ ___   _ | | _   __ _  _ __ ")
 print("| | | || | / _ \ | | / _ \| '_ \  / __|| | / _` | |  _|   / _` || '_ ` _ \ | || || | / _` || '__|")
 print("\ \_/ /| || (_) || ||  __/| | | || (__ | || (_| | | |    | (_| || | | | | || || || || (_| || |   ")
-print(" \___/ |_| \___/ |_| \___||_| |_| \___||_| \__,_| \_|     \__,_||_| |_| |_||_||_||_| \__,_||_|   ")
-time.sleep(0.5)                                                                                                 
+print(" \___/ |_| \___/ |_| \___||_| |_| \___||_| \__,_| \_|     \__,_||_| |_| |_||_||_||_| \__,_||_|   ")                                                                                              
 print(" _   _         _  _         1       _____                        ")
 print("| | | |       | || |              |_   _|                        ")
 print("| |_| |  __ _ | || |  ___  _   _    | |    ___   __ _  _ __ ___  ")
@@ -16,8 +15,8 @@ print("| | | || (_| || || ||  __/| |_| |   | |  |  __/| (_| || | | | | |")
 print(" _| |_/ \__,_||_||_| \___| \__, |   \_/   \___| \__,_||_| |_| |_|")
 print("                            __/ |                                ")
 print("                           |___/                                 ")
-time.sleep(1)
-os.system('cls')
+time.sleep(2)
+
 
 print("   ___                      _                             ___                    _                       ___ ")
 print("  / _ \   _  _   ___     __| |  ___   ___  ___   __ _    | _ \  ___   ___  ___  | | __ __  ___   _ _    |__ \ ")
@@ -28,12 +27,16 @@ print ("1 - RESOLUCION DE PROHIBICION DE ACERCAMIENTO")
 print ("2 - RESOLUCION DE EXCLUSION DE HOGAR")
 print ("3 - RESOLUCION CONFIRMACION")
 print ("4 - RESOLUCION DESESTIMATORIA ")
+os.system('cls')
 
-#opcion=input("Ingrese Opcion: ")
-#if opcion=="1":
-#	import prohibicion
-#	os.system ("cls")
-#	prohibicion.prohibicion()
+
+opcion=input("Ingrese Opcion: ")
+if opcion=="1":
+	import prohibicion
+	os.system ("cls")
+prohibicion.prohibicion()
+
+
 #if opcion=="2":
 #	print ("EN PROCESO")
 
